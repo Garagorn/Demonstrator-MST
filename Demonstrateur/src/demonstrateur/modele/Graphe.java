48d1816dd5cf86071537347ca714227e14205753
Basile Tellier <tellier212@CAMPUS@V304V-NCP042.campus.unicaen.fr>
2025-10-19 18:19:15 +0200

    MST avec Prim

--- Demonstrateur/src/demonstrateur/modele/Graphe.java
+++ Demonstrateur/src/demonstrateur/modele/Graphe.java
@@ -88,6 +88,98 @@ public class Graphe {
         }
         return g;
     }
+/**
+    fonction prim(G, s)
+           pour tout sommet t
+                  cout[t] := +∞
+                  pred[t] := null
+           cout[s] := 0
+           F := file de priorité contenant les sommets de G avec cout[.] comme priorité 
+           tant que F ≠ vide
+                t := F.defiler
+                pour toute arête t--u avec u appartenant à F
+                    si cout[u] >= poids de l'arête entre les sommets t et u
+                           pred[u] := t
+                           cout[u] := poids de l'arête entre les sommets t et u
+                           F.notifierDiminution(u)
 
+           retourner pred
+ */
+     
+     /**
+      * Méthode de calcul d'un Arbre couvrant minimal
+      * @param depart Sommet de départ du traitement
+      * @return Arbre Résultant du traitement du graphe    
+      */
+    public Arbre Prim(Sommet depart){
+        Arbre prim = new Arbre();
+        List<Sommet> sommets = getSommets();
+         
+        Map<Sommet, Integer> cout = new HashMap<>();
+        Map<Sommet, Sommet> pred = new HashMap<>();
+        //Parcours des sommets
+        for (Sommet t : sommets) {
+            cout.put(t, Integer.MAX_VALUE);
+            pred.put(t, null);
+        }
+        cout.put(depart, 0);
 
+        // File de priorité (triée sur le cout)
+        PriorityQueue<Sommet> file = new PriorityQueue<>(Comparator.comparingInt(cout::get));
+        file.addAll(sommets);
+
+        // Boucle principale
+        while (!file.isEmpty()) {
+            Sommet t = file.poll(); // sommet avec le plus petit cout
+
+            // Si t a un prédécesseur, on ajoute l'arête correspondante au MST
+            Sommet parent = pred.get(t);
+            if (parent != null) {
+                Arete a = getArete(parent, t);
+                if (a != null) {
+                    prim.ajouterArete(a);
+                }
+            }
+
+            // Exploration des voisins de t
+            for (Arete a : getAretes()) {
+                Sommet u = null;
+                if (a.getU().equals(t))
+                    u = a.getV();
+                else if (a.getV().equals(t))
+                    u = a.getU();
+
+                if (u != null && file.contains(u)) {
+                    int poids = a.getPoids();
+                    if (poids < cout.get(u)) {
+                        cout.put(u, poids);
+                        pred.put(u, t);
+
+                        // Mise à jour de la priorité (on retire et on réinsère)
+                        file.remove(u);
+                        file.add(u);
+                    }
+                }
+            }
+        }
+
+        return prim;
+    }
+
+    /**
+     * Getter de l'arete etre deux sommets
+     * @param s1 1er Sommet
+     * @param s2 2e sommet
+     * @return Arete L'arete entre le sommet S1 et S2
+     */
+    private Arete getArete(Sommet s1, Sommet s2) {
+        for (Arete a : aretes) {
+            if ((a.getU().equals(s1) && a.getV().equals(s2)) ||
+                (a.getU().equals(s2) && a.getV().equals(s1))) {
+                return a;
+            }
+        }
+        return null;
+    }
+    
 }
